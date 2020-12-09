4cac5b48ba2ce3510ab9c9bb1ecbb16e71fdef4f
罗杨 <lyfromyc@foxmail.com>
2020-12-09 15:03:07 +0800

    用户数据库

---
+++ db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `user` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_name_uindex` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4;
